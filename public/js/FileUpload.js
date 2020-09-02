93c71fd5bd4de8d5c147f1f1fcebb5e7d54f9068
Ivana Andersson <ivana.p.kostova@gmail.com>
2020-09-02 18:19:10 +0300

    add styles for the new recipe page

--- public/js/FileUpload.js
+++ public/js/FileUpload.js
@@ -4,10 +4,9 @@ FilePond.registerPlugin(
   FilePondPluginFileEncode
 );
 
-// FilePond.setOptions({
-//   stylePanelAspectRatio: 150 / 100,
-//   imageResizeTargetWidth: 100,
-//   imageResizeTargetHeight: 150,
-// });
+FilePond.setOptions({
+  stylePanelAspectRatio: 0.5,
+});
 
+//Turn all file input elements into ponds
 FilePond.parse(document.body);
